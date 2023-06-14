35a9ad42c7870f9431216789086eebec2de70b28
Andreas Poehlmann <ap--@users.noreply.github.com>
2023-06-14 21:36:28 +0200

    fix import

--- concatenated_zlib/__init__.py
+++ concatenated_zlib/__init__.py
@@ -1,5 +1,5 @@
-from _zlib import zlib_decode as zlib_concat_decode
-from _zlibng import zlibng_decode as zlibng_concat_decode
+from ._zlib import zlib_decode as zlib_concat_decode
+from ._zlibng import zlibng_decode as zlibng_concat_decode
 
 try:
     from ._version import __version__
