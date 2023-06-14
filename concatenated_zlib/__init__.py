c0ab1053ee7e5fa4c725bb6e29ae380f1413be18
Andreas Poehlmann <ap--@users.noreply.github.com>
2023-06-14 21:26:12 +0200

    import functions in toplevel module

---
+++ concatenated_zlib/__init__.py
@@ -0,0 +1,7 @@
+from _zlib import zlib_decode as zlib_concat_decode
+from _zlibng import zlibng_decode as zlibng_concat_decode
+
+__all__ = [
+    "zlib_concat_decode",
+    "zlibng_concat_decode",
+]
