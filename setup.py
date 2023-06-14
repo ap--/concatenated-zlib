80674cad749277ac88e84c8303f823e169d603af
Andreas Poehlmann <ap--@users.noreply.github.com>
2023-06-14 20:13:02 +0200

    format setup.py

--- setup.py
+++ setup.py
@@ -5,29 +5,31 @@ from setuptools.extension import Extension
 
 
 setup(
-    name='concatenated_zlib',
+    name="concatenated_zlib",
     version="v0.0.1",
     license="BSD",
     description="abc",
-    python_requires='>=3.8',
-    packages=['concatenated_zlib'],
+    python_requires=">=3.8",
+    packages=["concatenated_zlib"],
     ext_modules=cythonize(
         [
-        Extension(
-            "concatenated_zlib._shared",
-            sources=[
-                "concatenated_zlib/_shared.pyx"
-            ],
-            include_dirs=[numpy.get_include()]
-        ),
-        Extension(
-            "concatenated_zlib._zlib",
-            sources=[
-                "concatenated_zlib/_zlib.pyx"
-            ],
-            libraries=["z"],
-            include_dirs=[numpy.get_include()]
-        ),
+            Extension(
+                "concatenated_zlib._shared",
+                sources=["concatenated_zlib/_shared.pyx"],
+                include_dirs=[numpy.get_include()],
+            ),
+            Extension(
+                "concatenated_zlib._zlib",
+                sources=["concatenated_zlib/_zlib.pyx"],
+                libraries=["z"],
+                include_dirs=[numpy.get_include()],
+            ),
+            Extension(
+                "concatenated_zlib._zlibng",
+                sources=["concatenated_zlib/_zlibng.pyx"],
+                libraries=["z-ng"],
+                include_dirs=[numpy.get_include()],
+            ),
         ],
         compiler_directives={
             "language_level": 3,
